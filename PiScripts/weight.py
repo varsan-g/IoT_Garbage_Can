417e6ff3d8e423e50e2d7db2967c524cc838e3bb
Srivarsan Gnanachandran <mail@varsan.dk>
2024-03-11 12:15:43 +0100

    Typos fixed

--- PiScripts/weight.py
+++ PiScripts/weight.py
@@ -4,14 +4,14 @@ from hx711 import HX711
 try:
     GPIO.setmode(GPIO.BCM)
     hx = HX711(dout_pin=5, pd_sck_pin=6)
-    print(" please don't place anything on the weigth")
+    print("Please don't place anything on the weigth...")
     hx.zero() #reset the hx711
     initial_reading = hx.get_raw_data_mean() #get value without weight
-    input("Put a known weight on the weight and press enter")
+    input("Put a known weight on the weight and press enter: ")
     cali_reading =hx.get_data_mean()
-    known_weight = input("How much weight did you put on in gr")
+    known_weight = input("How much weight did you put on in gr: ")
     hx.set_scale_ratio(cali_reading/float(known_weight)) #set the ratio to the value change for each gram
-    input ("calibration done, press Enter to read weight values")
+    input ("calibration done, press Enter to read weight values: ")
     while True:
        print(hx.get_weight_mean(20), "g") # print the value in gram
 finally:
