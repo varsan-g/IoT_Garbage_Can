6ba8d776a6913d14f0e263f310a6ba2457d16266
SunEsk <sune.eskildsen@gmail.com>
2024-03-13 11:32:49 +0100

    Weight.py working

--- PiScripts/weight.py
+++ PiScripts/weight.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 from hx711 import HX711
 from websocket import create_connection
+from datetime import datetime
 
 # Set up the correct pin numbers here
 DT_PIN = 5 
@@ -10,9 +11,9 @@ G_DIODE_PIN = 18 # Green diode pin
 SWITCH_PIN = 0 # Button(Switch) pin
 
 weight_before_reset = 0
-weight_reset_threshold = 500
+weight_reset_threshold = 250
 last_weight = None
-was_above_threshold = False
+above_threshold = False
 
 
 def reset():
@@ -30,33 +31,31 @@ try:
     initial_reading = hx.get_raw_data_mean() #get value without weight
     ws = create_connection ("ws://10.176.69.102:8080")
     ws.send("connected")
-    input("Put a known weight on the weight and press enter: ")
+    input("Put a known weight on the weight and PRESS ENTER ")
     cali_reading =hx.get_data_mean()
-    known_weight = input("How much weight did you put on in gr: ")
+    known_weight = input("How much weight did you put on in grams: ")
     hx.set_scale_ratio(cali_reading/float(known_weight)) #set the ratio to the value change for each gram
-    input ("calibration done, press Enter to read weight values: ")
+    input ("Calibration done. PRESS ENTER ")
+    ws.send("TrashID:4")
+    ws.send("Location:McDonalds Sønderborg")
+    ws.send("Weight,TimeStamp")
     while True:
-       calculated_weight = hx.get_weight_mean(20)
-       print (calculated_weight)
-       while calculated_weight >= weight_reset_threshold:
-          if calculated_weight >= weight_reset_threshold:
+      calculated_weight = hx.get_weight_mean(20)
+      print(calculated_weight)
+      current_datetime = datetime.now()
+      if calculated_weight >= weight_reset_threshold:
             last_weight = calculated_weight
-<<<<<<< HEAD
-          elif last_weight and calculated_weight < 50:
-=======
-            was_above_threshold = True
-          elif was_above_threshold and calculated_weight < 50:
->>>>>>> 9a1fd43ccbde32f3d3497c0e0bf8786e18ab5644
+            above_threshold = True
+      elif above_threshold and calculated_weight < 50:
             # Weight has been emptied, send the last weight above 500
             GPIO.output(R_DIODE_PIN, GPIO.HIGH)
             GPIO.output(G_DIODE_PIN, GPIO.LOW)
-            print("Last weight before emptying:", last_weight)
-            ws.send(str(last_weight))
-<<<<<<< HEAD
-=======
-            was_above_threshold = False
->>>>>>> 9a1fd43ccbde32f3d3497c0e0bf8786e18ab5644
+            print("Last weight:", last_weight)
+            ws.send(str(last_weight) + "," + str(current_datetime))
             last_weight = None  # Reset the last weight above threshold
+            above_threshold = False
+      else:
+            GPIO.output(R_DIODE_PIN, GPIO.LOW)
+            GPIO.output(G_DIODE_PIN, GPIO.HIGH)
 finally:
-        GPIO.cleanup()
-        #ws.close()
+    GPIO.cleanup()
